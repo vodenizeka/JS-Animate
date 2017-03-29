1d3668863061fda136d709ae27c28b6cb28185eb
vodenizeka <vodenizeka992@gmail.com>
2017-03-29 16:49:26 +0200

    more stuff

--- Bounce/balls.js
+++ Bounce/balls.js
@@ -1,19 +1,36 @@
 function Ball() {
 	this.radius = Math.random()*10 + 5;
-	this.mass = this.radius;
-	this.x = Math.random() * (canvas.width - this.radius);
-	this.y = Math.random() * (canvas.height - this.radius);
+	this.mass = this.radius * this.radius * Math.PI;
+	this.x = Math.random() * canvas.width;
+	this.y = Math.random() * canvas.height;
 	this.vx = Math.random() * maxSpeed;
 	this.vy = Math.random() * maxSpeed;
+	this.intensity = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
 	this.color = "hsla("+ Math.random()*360 +",100%,50%,1)";
-	this.intensity = Math.sqrt(this.vx*this.vx + this.vy*this.vy);
+	this.colorWheel = 0;
 }
 
-Ball.prototype.checkEdge = function() {
+Ball.prototype.draw = function() {
+	c.strokeStyle = this.color;
+	c.fillStyle = this.color;
+	c.beginPath();
+	c.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+	c.stroke();
+	c.fill();
+	if (glow) /////
+		c.fillStyle = "rgba(255,255,255," + glow + ")";
+};
+
+Ball.prototype.update = function() {
 	var rightEdge = canvas.width - this.radius,
 		leftEdge = this.radius,
 		topEdge = this.radius,
 		bottomEdge = canvas.height - this.radius;
+
+	this.x += this.vx;
+	this.y += this.vy;					
+	this.vy += gravity;
+
 	if (this.x >= rightEdge) {
 		this.x = rightEdge;
 		this.vx *= -1;
@@ -32,25 +49,85 @@ Ball.prototype.checkEdge = function() {
 	}
 };
 
-Ball.prototype.draw = function() {
-	this.x += this.vx;
-	this.y += this.vy;					
-	this.checkEdge();
-	var visual = this.color;
-	if (visualMode === "SPEED") {
-		var relSpeed = (this.intensity - speedAvg)/speedAvg;
-		if (relSpeed > 1) {
-			relSpeed = 1;	
+function calculateCollisions() {
+	for (var i = 0; i < ballNum-1; i++) {
+		for (var j = i +1; j < ballNum; j++) {
+			if (balls[i].isClose(balls[j])) {
+				if (balls[i].collide(balls[j]))
+					balls[i].updateVelocity(balls[j]);
+			}
 		}
-		visual = "hsla(" + (120 - 120*relSpeed) + ",100%, 50%, 1)";
 	}
-	c.fillStyle = visual;
-	c.strokeStyle = visual;
-	c.beginPath();
-	c.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
-	c.stroke();
-	c.fill();
+}
+
+Ball.prototype.isClose = function(ball) {
+	var right1 = this.x + this.radius,
+		left1 = this.x - this.radius,
+		top1 = this.y - this.radius,
+		bot1 = this.y + this.radius,
+		right2 = ball.x + ball.radius,
+		left2 = ball.x - ball.radius,
+		top2 = ball.y - ball.radius,
+		bot2 = ball.y + ball.radius;
+
+	if (right1 > left2 && left1 < right2 && bot1 > top2 && top1 < bot2)
+		return true;
+
+	return false;
+}
+
+Ball.prototype.collide = function(ball) {
+	var realDist;
+	if ((realDist = this.distance(ball)) <= this.radius + ball.radius) {
+		var vector = [ball.x - this.x, ball.y - this.y],
+			distanceDiff = this.radius + ball.radius - realDist,
+			norm = [vector[0]/realDist, vector[1]/realDist];
+
+		this.x -= norm[0] * distanceDiff/2;
+		this.y -= norm[1] * distanceDiff/2;
+		ball.x += norm[0] * distanceDiff/2;
+		ball.y += norm[1] * distanceDiff/2;
+
+		return true;
+	}
+	return false;
 };
+
+Ball.prototype.updateVelocity = function(ball) {
+	var vector = [ball.x - this.x, ball.y - this.y],
+		intensity = this.radius + ball.radius,
+		norm = [vector[0]/intensity, vector[1]/intensity];
+
+	u1 = this.vx * norm[0] + this.vy * norm[1], 
+	u2 = ball.vx * norm[0] + ball.vy * norm[1], 
+	v1 = (u1*(this.mass - ball.mass) + 2*ball.mass*u2) / (this.mass + ball.mass), 
+	v2 = (u2*(ball.mass - this.mass) + 2*this.mass*u1) / (this.mass + ball.mass); 
+
+	this.vx += norm[0]*(v1-u1);
+	this.vy += norm[1]*(v1-u1);
+	ball.vx += norm[0]*(v2-u2);
+	ball.vy += norm[1]*(v2-u2);
+
+	this.intensity = Math.sqrt(this.vx*this.vx + this.vy*this.vy);
+	ball.intensity = Math.sqrt(ball.vx*ball.vx + ball.vy*ball.vy);
+
+	if (visualMode === "SPEED") {
+		this.changeColor();
+		ball.changeColor();
+	}
+};
+
+Ball.prototype.changeColor = function() {
+	var relSpeed = (this.intensity - speedAvg) / speedAvg;
+	if (relSpeed > 1) 
+		relSpeed = 1;	
+	this.colorWheel = relSpeed;
+	this.color = "hsla(" + (120 - 120*relSpeed) + ",100%, 50%, 1)";
+};
+
+Ball.prototype.distance = function(ball) {
+	return Math.sqrt((this.x-ball.x)*(this.x-ball.x) + (this.y-ball.y)*(this.y-ball.y));
+};	
 
 function initBalls() {
 	for (var i = 0; i < setBallNum; i++) {
@@ -62,58 +139,43 @@ function initBalls() {
 }
 
 function deleteBalls() {
-	for (var i = 0; i < setBallNum; i++) {
+	for (var i = 0; i < setBallNum; i++) 
 		delete balls[i];
-	}	
 	ballNum = 0;
 	speedSum = 0;
 }	
 
-function distance(x, y, x1, y1) {
-	return Math.sqrt((x-x1)*(x-x1) + (y-y1)*(y-y1));
-}	
-
-function collide(ball1, ball2) {
-	var tmp;
-	if ((tmp = distance(ball1.x, ball1.y, ball2.x, ball2.y))
-				 <= ball1.radius + ball2.radius) {
-		var vektor = [ball2.x - ball1.x, ball2.y - ball1.y],
-			distanceDiff = ball1.radius + ball2.radius - tmp,
-			norm = [vektor[0]/tmp, vektor[1]/tmp];
-		ball1.x -= norm[0] * distanceDiff/2;
-		ball1.y -= norm[1] * distanceDiff/2;
-		ball2.x += norm[0] * distanceDiff/2;
-		ball2.y += norm[1] * distanceDiff/2;
-		
-		return true;
+function prebroj() {
+	var colorNum = [0,0,0,0];
+	for (var i = 0; i < ballNum; i++) {
+		var tmp = balls[i].colorWheel;
+		if (tmp >= -1 && tmp <= -0.5) 
+			colorNum[3]++;
+		else if (tmp > -0.5 && tmp <= 0)
+			colorNum[2]++;
+		else if (tmp > 0 && tmp <= 0.5)
+			colorNum[1]++;
+		else if (tmp > 0.5 && tmp <= 1)
+			colorNum[0]++;
 	}
-	return false;
+	document.getElementById("red").innerHTML = colorNum[0];	
+	document.getElementById("yellow").innerHTML = colorNum[1];	
+	document.getElementById("green").innerHTML = colorNum[2];	
+	document.getElementById("blue").innerHTML = colorNum[3];	
 }
 
-function updateVelocity(ball1, ball2) {
-	var vektor = [ball2.x - ball1.x, ball2.y - ball1.y],
-		intensity = ball1.radius + ball2.radius,
-		norm = [vektor[0]/intensity, vektor[1]/intensity],
-		u1 = ball1.vx * norm[0] + ball1.vy * norm[1], // vektor1 before coll
-		u2 = ball2.vx * norm[0] + ball2.vy * norm[1], // vektor2 before coll
-		v1 = (u1*(ball1.mass - ball2.mass) + 2*ball2.mass*u2) /
-			 (ball1.mass + ball2.mass),             // vektor1 after coll
-		v2 = (u2*(ball2.mass - ball1.mass) + 2*ball1.mass*u1) /
-			 (ball1.mass + ball2.mass);             // vektor1 after coll	
-		ball1.vx += norm[0]*(v1-u1)
-		ball1.vy += norm[1]*(v1-u1)
-		ball2.vx += norm[0]*(v2-u2)
-		ball2.vy += norm[1]*(v2-u2)
-		ball1.intensity = Math.sqrt(ball1.vx*ball1.vx + ball1.vy*ball1.vy);
-		ball2.intensity = Math.sqrt(ball2.vx*ball2.vx + ball2.vy*ball2.vy);
-}
-
-function calculateCollisions() {
-	for (var i = 0; i < ballNum-1; i++) {
-		for (var j = i +1; j < ballNum; j++) {
-			if (collide(balls[i], balls[j]))
-				updateVelocity(balls[i], balls[j]);
-		}
+function hideElement(element, button) {
+	if (element.style.display != "none") {
+		element.style.display = "none";
+		button.value = "show";
+	}
+	else {
+		element.style.display = "block";
+		button.value = "hide";
 	}
 }
 
+function reColor() {
+	for (var i = 0; i < ballNum; i++)
+		balls[i].changeColor();
+}
